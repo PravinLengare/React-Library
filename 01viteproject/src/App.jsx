62d9f9f26ed2221cdb110b57bc7c07b0bca7c839
PravinLengare <lengarepravin115@gmail.com>
2026-02-05 11:03:05 +0530

    manupulating the project

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -1,9 +1,11 @@
-
+import Test from './Test.jsx'
 function App() {
 
   return (
+   <>
    <h2>Hi react!</h2>
+   <Test />
+   </>
   )
 }
-
 export default App
---
+++ 01viteproject/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test(){
+    return (
+    <>
+        <h1>By changing !!!!</h1>
+    </>
+        
+    )
+}
+export default Test
