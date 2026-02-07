08a1239dc5e2d496b860c7fd81ef0fb03f7b8f95
PravinLengare <lengarepravin115@gmail.com>
2026-02-07 23:58:02 +0530

    added tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl text-green-400 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  )
+}
